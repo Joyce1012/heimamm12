02df080255330320e394c07f013ad4551600714b
joyce <joyce@qq.com>
2020-04-06 16:18:43 +0800

    登录模块处理

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -7,6 +7,42 @@
         <span class="titleLine">|</span>
         <span class="titleName2">用户登录</span>
       </div>
+      <el-form :model="form" :rules="rules" ref="form">
+        <el-form-item prop="phone">
+          <el-input prefix-icon="el-icon-user" v-model="form.phone" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="el-icon-lock"
+            :show-password="true"
+            v-model="form.password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-row>
+            <el-col :span="16">
+              <el-input prefix-icon="el-icon-key" v-model="form.code" placeholder="请输入验证码"></el-input>
+            </el-col>
+            <el-col :span="8">
+              <img src="@/assets/img/code.png" alt class="code" />
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="form.isCheck">
+            我已阅读并同意
+            <el-link type="primary">用户协议</el-link>和
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" class="btn" @click="submit">登录</el-button>
+          <br />
+          <el-button type="primary" class="btn" @click="reset">注册</el-button>
+        </el-form-item>
+      </el-form>
     </div>
     <div class="right">
       <img src="@/assets/img/login_banner_ele.png" alt />
@@ -15,15 +51,52 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+       form: {
+        phone: "",
+        password: "",
+        code: "",
+        isCheck: ""
+      },
+      rules: {
+        phone: [
+          { required: true, message: "请输入手机号", trigger: "change" },
+          { min: 11, max: 11, message: "请正确输入手机号", trigger: "change" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "change" },
+          { min: 6, max: 12, message: "请输入6-12位长度密码", trigger: "change" }
+        ],
+        code: [
+          { required: true, message: "请输入验证码", trigger: "change" },
+          { min: 4, max: 4, message: "请正确输入验证码", trigger: "change" }
+        ]
+      }
+    }
+  },
+   methods: {
+    submit(){
+      this.$refs.form.validate(result=>{
+        this.$message.success(result + '');
+      });
+    },
+    reset(){
+      this.$refs.form.resetFields();
+    }
+  },
+ 
+};
 </script>
 
 <style lang="less">
 .login {
+  width: 1400px;
+  height: 788px;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  height: 100%;
   background: linear-gradient(
     225deg,
     rgba(20, 147, 250, 1),
@@ -34,26 +107,39 @@ export default {};
     height: 550px;
     background: rgba(245, 245, 245, 1);
     padding: 43px;
-    .titleName {
-      width: 94px;
-      height: 22px;
-      font-size: 24px;
-      font-weight: 400;
-      color: rgba(12, 12, 12, 1);
-      margin-left: 15px;
-      margin-right: 15px;
+    .btn {
+      width: 100%;
     }
-    .titleLine {
-      color: #ccc;
-      font-size: 24px;
+    .btn:nth-child(1) {
+      margin-bottom: 26px;
     }
-    .titleName2 {
-      width: 87px;
-      height: 21px;
-      font-size: 22px;
-      font-weight: 400;
-      color: rgba(12, 12, 12, 1);
-      margin-left: 15px;
+    .code {
+      width: 100%;
+      height: 40px;
+    }
+    .title {
+      margin-bottom: 20px;
+      .titleName {
+        width: 94px;
+        height: 22px;
+        font-size: 24px;
+        font-weight: 400;
+        color: rgba(12, 12, 12, 1);
+        margin-left: 15px;
+        margin-right: 15px;
+      }
+      .titleLine {
+        color: #ccc;
+        font-size: 24px;
+      }
+      .titleName2 {
+        width: 87px;
+        height: 21px;
+        font-size: 22px;
+        font-weight: 400;
+        color: rgba(12, 12, 12, 1);
+        margin-left: 15px;
+      }
     }
   }
 }
